8f2c0265c5124daf61b6fe7751129e8bb290505a
Mitchie97 <mitchelllebogang@gmail.com>
2024-04-04 06:20:24 -0700

    Attempt 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 
-for ascii_value in range(ord('a'), ord('z') + 1):
-    print(chr(ascii_value), end=" ")
+print("".join(["{}".format(chr(i)) for i in range(ord('a'), ord('z') + 1)]))
